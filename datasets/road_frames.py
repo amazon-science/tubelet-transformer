e400b8bdce54e3e48a5688278e2441a2787bab09
Salman Khan <salmank255@gmail.com>
2023-04-05 18:49:07 +0100

    roadpp

--- datasets/road_frames.py
+++ datasets/road_frames.py
@@ -663,7 +663,9 @@ class VideoDataset(tutils.data.Dataset):
         self._transforms = transform
         self.ids = list()
         if self.DATASET == 'road':
-            self._make_lists_road()  
+            self._make_lists_road()
+        elif self.DATASET == 'roadpp':
+            self._make_lists_roadpp()
         elif self.DATASET == 'ucf24':
             self._make_lists_ucf24() 
         else:
@@ -769,6 +771,157 @@ class VideoDataset(tutils.data.Dataset):
         self.print_str = ptrstr
         
     
+    def _make_lists_roadpp(self):
+        
+        # if self.MODE =='train':
+        #     self.anno_file  = os.path.join(self.root, 'road_plus_plus_trainval_v1.0.json')
+        # else:
+        #     self.anno_file  = os.path.join(self.root, 'road_plus_plus_test_v1.0.json')
+        
+        self.anno_file  = os.path.join(self.root, 'road_plus_plus_trainval_v1.0.json')
+        with open(self.anno_file,'r') as fff:
+            final_annots = json.load(fff)
+        
+        database = final_annots['db']
+        
+        # self.label_types =  final_annots['label_types'] #['agent', 'action', 'loc', 'duplex', 'triplet'] #
+        self.label_types = ['agent', 'action', 'loc'] #
+        # print(self.label_types)
+        # print(rr)
+
+        num_label_type = len(self.label_types)
+        self.num_classes = 1 ## one for presence
+        self.num_classes_list = [1]
+        for name in self.label_types: 
+            logger.info('Number of {:s}: all :: {:d} to use: {:d}'.format(name, 
+                len(final_annots['all_'+name+'_labels']),len(final_annots[name+'_labels'])))
+            numc = len(final_annots[name+'_labels'])
+            self.num_classes_list.append(numc)
+            self.num_classes += numc
+        
+        self.ego_classes = final_annots['av_action_labels']
+        self.num_ego_classes = len(self.ego_classes)
+
+        # counts = np.zeros((len(final_annots[self.label_types[-1] + '_labels']), num_label_type), dtype=np.int32)
+        counts = np.zeros((len(final_annots[self.label_types[0] + '_labels']) + len(final_annots[self.label_types[1] + '_labels']) +len(final_annots[self.label_types[2] + '_labels'])  , num_label_type), dtype=np.int32)
+
+
+        self.video_list = []
+        self.numf_list = []
+        frame_level_list = []
+
+        for videoname in sorted(database.keys()):
+            # print(is_part_of_subsets(final_annots['db'][videoname]['split_ids'], self.SUBSETS))
+            if not is_part_of_subsets(final_annots['db'][videoname]['split_ids'], self.SUBSETS):
+                continue
+            
+            numf = database[videoname]['numf']
+            self.numf_list.append(numf)
+            self.video_list.append(videoname)
+            
+            frames = database[videoname]['frames']
+            # print(numf)
+            frame_level_annos = [ {'labeled':False,'ego_label':-1,'boxes':np.asarray([]),'labels':np.asarray([])} for _ in range(numf)]
+
+            frame_nums = [int(f) for f in frames.keys()]
+            frames_with_boxes = 0
+            for frame_num in sorted(frame_nums): #loop from start to last possible frame which can make a legit sequence
+                frame_id = str(frame_num)
+                if frame_id in frames.keys() and frames[frame_id]['annotated']>0:
+                    
+                    frame_index = frame_num-1  
+                    frame_level_annos[frame_index]['labeled'] = True 
+                    # frame_level_annos[frame_index]['ego_label'] = frames[frame_id]['av_action_ids'][0]
+                    
+                    frame = frames[frame_id]
+                    if 'annos' not in frame.keys():
+                        frame = {'annos':{}}
+                    
+                    all_boxes = []
+                    all_labels = []
+                    frame_annos = frame['annos']
+                    # temp_img = cv2.imread('../roadpp/rgb-images/'+videoname+'/{:05d}.jpg'.format(frame_num))
+                    for key in frame_annos:
+                        width, height = frame['width'], frame['height']
+                        anno = frame_annos[key]
+                        box = anno['box']
+                        
+                        assert box[0]<box[2] and box[1]<box[3], box
+                        assert width==1920 and height==1280, (width, height, box)
+                        
+                        # temp_img = cv2.rectangle(temp_img, (int(box[0]*1920),int(box[1]*1280)), (int(box[2]*1920),int(box[3]*1280)), (255,0,0), 2)
+                        # cv2.imwrite('temp_img.png',temp_img)
+                        for bi in range(4):
+                            assert 0<=box[bi]<=1.01, box
+                            box[bi] = min(1.0, max(0, box[bi]))
+                        
+                        all_boxes.append(box)
+                        box_labels = np.zeros(self.num_classes-1)
+                        list_box_labels = []
+                        cc = 1
+                        for idx, name in enumerate(self.label_types):
+                            # print(idx,name)
+                            filtered_ids = filter_labels(anno[name+'_ids'], final_annots['all_'+name+'_labels'], final_annots[name+'_labels'])
+                            list_box_labels.append(filtered_ids)
+                            for fid in filtered_ids:
+                                box_labels[fid+cc-1] = 1
+                                box_labels[0] = 1
+                            cc += self.num_classes_list[idx+1]
+
+                        all_labels.append(box_labels)
+
+                        # for box_labels in all_labels:
+                        for k, bls in enumerate(list_box_labels):
+                            for l in bls:
+                                counts[l, k] += 1 
+                    # print(videoname,frame_num)
+                    # print(rr)
+                    all_labels = np.asarray(all_labels, dtype=np.float32)
+                    all_boxes = np.asarray(all_boxes, dtype=np.float32)
+
+                    if all_boxes.shape[0]>0:
+                        frames_with_boxes += 1    
+                    frame_level_annos[frame_index]['labels'] = all_labels
+                    frame_level_annos[frame_index]['boxes'] = all_boxes
+
+            logger.info('Frames with Boxes are {:d} out of {:d} in {:s}'.format(frames_with_boxes, numf, videoname))
+            frame_level_list.append(frame_level_annos)  
+
+            ## make ids
+            start_frames = [ f for f in range(numf-self.MIN_SEQ_STEP*self.SEQ_LEN, 1,  -self.skip_step)]
+            if self.full_test and 1 not in start_frames:
+                start_frames.append(1)
+            logger.info('number of start frames: '+ str(len(start_frames)))
+            for frame_num in start_frames:
+                step_list = [s for s in range(self.MIN_SEQ_STEP, self.MAX_SEQ_STEP+1) if numf-s*self.SEQ_LEN>=frame_num]
+                shuffle(step_list)
+                # print(len(step_list), self.num_steps)
+                for s in range(min(self.num_steps, len(step_list))):
+                    video_id = self.video_list.index(videoname)
+                    if len(frame_level_list[video_id][frame_num+int(self.SEQ_LEN/2)]['boxes']) >0:
+                            self.ids.append([video_id, frame_num ,step_list[s]])
+
+        # pdb.set_trace()
+        ptrstr = ''
+        self.frame_level_list = frame_level_list
+        self.all_classes = [['agent_ness']]
+        for k, name in enumerate(self.label_types):
+            labels = final_annots[name+'_labels']
+            self.all_classes.append(labels)
+            # self.num_classes_list.append(len(labels))
+            for c, cls_ in enumerate(labels): # just to see the distribution of train and test sets
+                ptrstr += '-'.join(self.SUBSETS) + ' {:05d} label: ind={:02d} name:{:s}\n'.format(
+                                                counts[c,k] , c, cls_)
+        
+        ptrstr += 'Number of ids are {:d}\n'.format(len(self.ids))
+
+        self.label_types = ['agent_ness'] + self.label_types
+        self.childs = {'duplex_childs':final_annots['duplex_childs'], 'triplet_childs':final_annots['triplet_childs']}
+        self.num_videos = len(self.video_list)
+        self.print_str = ptrstr
+
+
+
     def _make_lists_road(self):
         
         self.anno_file  = os.path.join(self.root, 'road_trainval_v1.0.json')
